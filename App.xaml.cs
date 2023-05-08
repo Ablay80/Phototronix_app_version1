aab727fb2f55e435ad7d96d16114ff66a7bee87b
Ablay80 <ablaybektur80@gmail.com>
2023-05-08 17:08:12 +0600

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Phototronix_app_version1
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Sign_In.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Phototronix_app_version1
+{
+    /// <summary>
+    /// Логика взаимодействия для Sign_In.xaml
+    /// </summary>
+    public partial class Sign_In : Window
+    {
+        public Sign_In()
+        {
+            InitializeComponent();
+        }
+
+        private void passwordCheck_Unchecked(object sender, RoutedEventArgs e)
+        {
+            passwordBox.PasswordChar = '•';
+        }
+        private void passwordCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordBox.PasswordChar = (char)0;
+        }
+
+        private void Closebtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
